d8540966fda61a6e6e3388292014ce3becd296b3
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2025-04-27 17:09:45 +0300

    delete 2 files

--- func.py
+++
@@ -1,4 +0,0 @@
-numbers = [number for number in range(50)]
-print(numbers)
-yield_numbers = list((number for number in range(50)))
-print(yield_numbers)
--- new.py
+++
@@ -1,4 +0,0 @@
-numbers = [1, 2]
-print(id(numbers))
-numbers = [3, 'text']
-print(id(numbers))
